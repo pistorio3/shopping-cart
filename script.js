e552dbfdae3cb1aceb9121b919c1c9b4e34a1265
João Victor Pistorio <joao.pistorio@gmail.com>
2021-04-20 18:58:12 -0300

    Requisito 2

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Native Function
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -5,6 +6,7 @@ function createProductImageElement(imageSource) {
   return img;
 }
 
+// Native Function
 function createCustomElement(element, className, innerText) {
   const e = document.createElement(element);
   e.className = className;
@@ -12,6 +14,50 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
+// function cartItemClickListener(event) {
+//   // coloque seu código aqui
+// }
+
+// Native Function
+function createCartItemElement({ sku, name, salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  // li.addEventListener('click', cartItemClickListener);
+  return li;
+}
+
+// Native Function
+function getSkuFromProductItem(item) {
+  return item.querySelector('span.item__sku').innerText;
+}
+
+// Developed Function
+function sendToCart(product) {
+  const productCart = {
+    sku: product.id,
+    name: product.title,
+    salePrice: product.price,
+  };
+
+  const element = createCartItemElement(productCart);
+  const cart = document.querySelector('.cart__items');
+  cart.appendChild(element);
+}
+
+// Developed Function
+// Consulta a API e retorna informações do produto que foi clicado
+async function getProductById(event) {
+  const id = getSkuFromProductItem(event.target.parentNode);
+
+  const item = await fetch(`https://api.mercadolibre.com/items/${id}`)
+  .then((res) => res.json());
+
+  console.log(item);
+  sendToCart(item);
+}
+
+// Native Function
 function createProductItemElement({ sku, name, image }) {
   const section = document.createElement('section');
   section.className = 'item';
@@ -19,40 +65,25 @@ function createProductItemElement({ sku, name, image }) {
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'))
+  .addEventListener('click', getProductById);
 
   return section;
 }
 
-// function getSkuFromProductItem(item) {
-//   return item.querySelector('span.item__sku').innerText;
-// }
-
-// function cartItemClickListener(event) {
-//   // coloque seu código aqui
-// }
-
-// function createCartItemElement({ sku, name, salePrice }) {
-//   const li = document.createElement('li');
-//   li.className = 'cart__item';
-//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-//   li.addEventListener('click', cartItemClickListener);
-//   return li;
-// }
-
-// New Functions
+// Developed Functions
 //----------------------------------------------------------------------------------------------------------------------
 
-// Cria os elementos HTML com auxílio das funções nativas
+// Cria os elementos HTML com auxílio das funções nativas do projeto
 function createProduct(products) {
   products.forEach((product) => {
-    const computer = {
+    const productobj = {
       sku: product.id,
       name: product.title,
       image: product.thumbnail,
     };
 
-    const element = createProductItemElement(computer);
+    const element = createProductItemElement(productobj);
     const section = document.querySelector('.items');
 
     section.appendChild(element);
@@ -60,7 +91,7 @@ function createProduct(products) {
 }
 
 // Consulta a API do ML
-async function getComputers() {
+async function getProducts() {
   const products = await fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
   .then((res) => res.json());
 
@@ -70,5 +101,5 @@ async function getComputers() {
 
 // Chamada da função de consulta ao carregar a página
 window.onload = function onload() { 
-  getComputers(); 
+  getProducts(); 
 };
